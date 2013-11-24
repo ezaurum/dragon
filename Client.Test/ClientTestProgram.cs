8092bf8af21f8bc6804b8f1e32e4c55e89f599ce
조석규 <ezaurum@gmail.com>
2013-11-24 22:42:54 +0900

    add dummy

--- Client.Test/ClientTestProgram.cs
+++ Client.Test/ClientTestProgram.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Sockets;
+using Dragon;
 using Dragon.Client;
 
 namespace Client.Test
@@ -7,10 +9,50 @@ namespace Client.Test
     {
         static void Main(string[] args)
         {
-            SocketConnector c = new SocketConnector();
+            SocketAsyncEventArgs socketAsyncEventArgs = new SocketAsyncEventArgs();
+            IAuthorizationManager am = new DummyClientAuthorizationManager();
+            ISessionManager sm = new DummyClientSessionManager();
+            IActionController ac = new DummyClientActionController();
+
+            socketAsyncEventArgs.Completed += am.Login;
+            socketAsyncEventArgs.Completed += sm.RequestSession;
+            socketAsyncEventArgs.Completed += ac.Init;
+            
+            SocketConnector c = new SocketConnector
+            {
+                ConnectEventArgs = socketAsyncEventArgs
+            };
+
             c.Connect("127.0.0.1",10008);
             
             Console.ReadKey();
+        }
+    }
+
+    public class DummyClientActionController : IActionController
+    {
+        public void Init(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError != SocketError.Success) return;
+             Console.WriteLine("Init");
+        }
+    }
+
+    public class DummyClientSessionManager : ISessionManager
+    {
+        public void RequestSession(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError != SocketError.Success) return;
+            Console.WriteLine("request session");
+        }
+    }
+
+    public class DummyClientAuthorizationManager : IAuthorizationManager
+    {
+        public void Login(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError != SocketError.Success) return;
+            Console.WriteLine("login");
         }
     }
 }
