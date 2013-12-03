8bb008f2fb20e7138a9f538a8175cc42b60333a4
조석규 <ezaurum@gmail.com>
2013-12-03 17:40:24 +0900

    test changed

--- Client.Test/ClientTestProgram.cs
+++ Client.Test/ClientTestProgram.cs
@@ -14,18 +14,15 @@ namespace Client.Test
                 Console.WriteLine("Connected");
 
                 c.Send(new SimpleMessage());
+                c.Send(new SimpleMessage());
+                c.Send(new SimpleMessage());
             };
 
             c.Disconnected += OnDisconnected;
 
-            c.WriteCompleted += message =>
-            {
-                Console.WriteLine("Write c");
-            };
-
             c.ReadCompleted += message =>
             {
-                Console.WriteLine("Read");
+                Console.WriteLine("Read " +message);
             };
 
             c.WriteCompleted += message =>
@@ -42,6 +39,63 @@ namespace Client.Test
         private static void OnDisconnected(object sender, SocketAsyncEventArgs socketAsyncEventArgs)
         {
             Console.WriteLine("Disconnected");
+        }
+    }
+
+    // 게임 대기방 생성 요청 (client->server)	
+    public class SimpleMessage : IMessage
+    {
+        public int MessageType { get { return 1; } }
+        public DateTime PacketTime { get; set; }
+        public Byte BoardType;
+        public Byte PlayMode;
+        public char PlayType;
+
+        public byte[] ToByteArray()
+        {
+            byte[] bytes = new byte[Length];
+            int index = 0;
+            BitConverter.GetBytes(Length)
+            .CopyTo(bytes, index);
+            index += sizeof(Int16);
+            BitConverter.GetBytes((Int32)MessageType)
+            .CopyTo(bytes, index);
+            index += sizeof(Int32);
+            BitConverter.GetBytes((Int64)PacketTime.ToBinary())
+            .CopyTo(bytes, index);
+            index += sizeof(Int64);
+            bytes[index] = BoardType;
+            index++;
+            bytes[index] = PlayMode;
+            index++;
+            bytes[index] = (Byte)PlayType;
+            index++;
+            return bytes;
+        }
+
+        public void FromByteArray(byte[] bytes)
+        {
+            int index = 6;
+            PacketTime = DateTime.FromBinary(BitConverter.ToInt64(bytes, index));
+            index += sizeof(Int64);
+            BoardType = bytes[index];
+            index += sizeof(Byte);
+            PlayMode = bytes[index];
+            index += sizeof(Byte);
+            PlayType = (char) bytes[index];
+            index += sizeof(Byte);
+        }
+
+        public Int16 Length
+        {
+            get
+            {
+                return (Int16)(2 + sizeof(Int32) + sizeof(Int64) + sizeof(Byte) + sizeof(Byte) + sizeof(Byte));
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("MessageType: {0}, PacketTime: {1}, BoardType: {2}, PlayMode: {3}, PlayType: {4}, ", MessageType, PacketTime, BoardType, PlayMode, PlayType);
         }
     }
 
@@ -62,7 +116,7 @@ namespace Client.Test
         }
     }
 
-    public class SimpleMessage : IMessage
+    public class SimpleMessage2 : IMessage
     {
         public DateTime PacketTime { get; set; }
         public Guid TurnOwner { get; set; }
--- Server.Test/ServerTestProgram.cs
+++ Server.Test/ServerTestProgram.cs
@@ -26,11 +26,77 @@ namespace Server.Test
             {
                 //Something to test
                 var userToken = (ServerDragonSocket<SimpleMessage>) eventArgs.UserToken;
+
+                userToken.ReadCompleted += message =>
+                {
+                    Console.WriteLine("READ " + message);
+                    userToken.Send(message);
+                    Console.WriteLine("echo " + message);
+                };
+
+                userToken.Activate();
             };
 
             Thread.Sleep(1000);
             s.Start();
             Console.ReadKey();
+        }
+    }
+
+    // 게임 대기방 생성 요청 (client->server)	
+    public class SimpleMessage : IMessage
+    {
+        public int MessageType { get { return 1; } }
+        public DateTime PacketTime { get; set; }
+        public Byte BoardType;
+        public Byte PlayMode;
+        public char PlayType;
+
+        public byte[] ToByteArray()
+        {
+            byte[] bytes = new byte[Length];
+            int index = 0;
+            BitConverter.GetBytes(Length)
+            .CopyTo(bytes, index);
+            index += sizeof(Int16);
+            BitConverter.GetBytes((Int32)MessageType)
+            .CopyTo(bytes, index);
+            index += sizeof(Int32);
+            BitConverter.GetBytes((Int64)PacketTime.ToBinary())
+            .CopyTo(bytes, index);
+            index += sizeof(Int64);
+            bytes[index] = BoardType;
+            index++;
+            bytes[index] = PlayMode;
+            index++;
+            bytes[index] = (Byte)PlayType;
+            index++;
+            return bytes;
+        }
+
+        public void FromByteArray(byte[] bytes)
+        {
+            int index = 6;
+            PacketTime = DateTime.FromBinary(BitConverter.ToInt64(bytes, index));
+            index += sizeof(Int64);
+            BoardType = bytes[index];
+            index += sizeof(Byte);
+            PlayMode = bytes[index];
+            index += sizeof(Byte);
+            PlayType = (char)bytes[index];
+            index += sizeof(Byte);
+        }
+
+        public Int16 Length
+        {
+            get
+            {
+                return (Int16)(2 + sizeof(Int32) + sizeof(Int64) + sizeof(Byte) + sizeof(Byte) + sizeof(Byte));
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("MessageType: {0}, PacketTime: {1}, BoardType: {2}, PlayMode: {3}, PlayType: {4}, ", MessageType, PacketTime, BoardType, PlayMode, PlayType);
         }
     }
 
@@ -48,57 +114,6 @@ namespace Server.Test
             var simpleMessage = new SimpleMessage();
             simpleMessage.FromByteArray(d);
             return simpleMessage;
-        }
-    }
-
-    public class SimpleMessage : IMessage
-    {
-        public DateTime PacketTime { get; set; }
-        public Guid TurnOwner { get; set; }
-        public Int32 ResponseLimit;
-
-        public byte[] ToByteArray()
-        {
-            byte[] bytes = new byte[Length];
-            int index = 0;
-            BitConverter.GetBytes(Length)
-            .CopyTo(bytes, index);
-            index += sizeof(Int16);
-            BitConverter.GetBytes((Int64)PacketTime.ToBinary())
-            .CopyTo(bytes, index);
-            index += sizeof(Int64);
-            TurnOwner.ToByteArray()
-            .CopyTo(bytes, index);
-            index += 16;
-            BitConverter.GetBytes(ResponseLimit)
-            .CopyTo(bytes, index);
-            index += sizeof(Int32);
-            return bytes;
-        }
-
-        public void FromByteArray(byte[] bytes)
-        {
-            int index = 2;
-            PacketTime = DateTime.FromBinary(BitConverter.ToInt64(bytes, index));
-            index += sizeof(Int64);
-            byte[] tempTurnOwner = new byte[16];
-            Buffer.BlockCopy(bytes, index, tempTurnOwner, 0, 16);
-            TurnOwner = new Guid(tempTurnOwner);
-            index += 16;
-            ResponseLimit = BitConverter.ToInt32(bytes, index);
-            index += sizeof(Int32);
-        }
-
-        public Int16 Length
-        {
-            get
-            {
-                return (Int16)(2 + sizeof(Int64) + 16 + sizeof(Int32));
-            }
-        }
-        public override string ToString()
-        {
-            return string.Format("PacketTime: {0}, TurnOwner: {1}, ResponseLimit: {2}, ", PacketTime, TurnOwner, ResponseLimit);
         }
     }
 }
