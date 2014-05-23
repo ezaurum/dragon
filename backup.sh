22f6ad923da5ab943befcab7f27d0e4a570e3b53
조석규 <ezaurum@gmail.com>
2014-05-23 15:42:09 +0900

    remove backup script

--- backup.sh
+++
@@ -1,2 +0,0 @@
-date +%Y%m%d | xargs -i{} git bundle create {}.bundle --all ; mv *.bundle ~/Dropbox/workspace_backup/dragon/
-date +%Y%m%d | xargs -i{} git bundle create {}.bundle --all ; mv *.bundle ~/Dropbox/CUBEGames/DM/network/
