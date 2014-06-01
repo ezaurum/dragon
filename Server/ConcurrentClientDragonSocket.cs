6e455e9e354645b1228b85a0ab22b6fcffb66404
조석규 <ezaurum@gmail.com>
2014-06-01 16:06:13 +0900

    remove unused

--- Server/ConcurrentClientDragonSocket.cs
+++ Server/ConcurrentClientDragonSocket.cs
@@ -132,8 +132,6 @@ namespace Dragon
         /// <param name="e"></param>
         private void CheckReconnect(object sender, ElapsedEventArgs e)
         {
-            Console.WriteLine("check reconnect " + _connectEventArgs.SocketError);
-
             //failed. retry
             if (_connectEventArgs.SocketError != SocketError.Success && (0 == RetryLimit || RetryCount < RetryLimit))
             {
